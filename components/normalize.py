ee73774589a1f102077bdf5c73bbfc6ca2d7b86d
AndrewJSchoen <schoen.andrewj@gmail.com>
2016-07-26 09:31:50 -0500

    Normalize Component: Bug Fixes
    [1] Fixed a bug where lowest-level normalization expected a template
    file.
    [2] Producing error with replica catalog.

--- components/normalize.py
+++ components/normalize.py
@@ -1,6 +1,7 @@
 #!/bin/python
 from Pegasus.DAX3 import *
 import sys
+import pandas
 
 class normalize(object):
     """
@@ -8,7 +9,7 @@ class normalize(object):
     Uses DTI-TK to normalize subjects to an iteratively improved template, or a predefined one.
     matrix is assumed to be a pandas dataframe. "hierarchy" is a list referencing headers in the matrix.
     """
-    def __init__(self, matrix, hierarchy=["PROJECT", "ID"], name="Project", template=None, similarity_metric="NMI", species="Human", rigid=3, affine=3, diffeomorphic=6):
+    def __init__(self, matrix, hierarchy=["PROJECT", "ID"], name="Project", template=None, similarity_metric="NMI", species="Human", rigid=3, affine=3, diffeomorphic=6, transferflag=True):
         self.matrix = matrix
         self.hierarchy = hierarchy
         if "ID" not in self.hierarchy:
@@ -22,47 +23,93 @@ class normalize(object):
         self.rigid = rigid
         self.affine = affine
         self.diffeomorphic = diffeomorphic
+        self.files = {}
+        self.transferflag = transferflag
+        if "PROJECT" not in self.matrix.columns:
+            self.matrix["PROJECT"] = self.name
         try:
-            if "PROJECT" not in self.matrix.columns:
-                self.matrix["PROJECT"] = self.name
-            self.matrix = self.matrix[hierarchy + ["SPD"]]
+            self.matrix = self.matrix[self.hierarchy + ["SPD"]]
         except:
             print("The spreadsheet you supplied did not have columns for the hierarchy specified. Exiting.")
             sys.exit(1)
-        self.__plan__()
 
     def __plan_tier__(self, matrix, hierarchies, dax):
-        if len(hierarchies) > 2:
-            child_matrix_set = matrix[hierarchies[1:]]
-            for child_matrix in child_matrix_set.groupby(hierarchies[1])
-                self.__plan_tier__(child_matrix, hierarchies[1:], dax)
 
         #Plan normalization here
+        template_tier = hierarchies[0]
+        template_id = matrix[hierarchies[0]].unique()[0]
+        source_tier = hierarchies[1]
+        source_ids = list(matrix[matrix[template_tier] == template_id][source_tier])
+        ImageDim_Jobs = []
+        CreateTemplate_Jobs = []
+
+        #ImageDim
+        for source_id in source_ids:
+            imagedimjob = normalize_ImageDim(template_tier=template_tier, source_tier=source_tier, template_id=template_id, source_id=source_id, hierarchy=hierarchies, matrix=matrix, transferflag=self.transferflag)
+            self.files.update(imagedimjob.files)
+            ImageDim_Jobs.append(imagedimjob)
+
+        #CreateTemplate
+        createtemplatejob = normalize_CreateTemplate(template_tier, source_tier, template_id, hierarchies, matrix, self.transferflag)
+        self.files.update(createtemplatejob.files)
+        CreateTemplate_Jobs.append(createtemplatejob)
+
+        for ImageDim_Job in ImageDim_Jobs:
+            dax.addJob(ImageDim_Job.pegasus_job)
+        for CreateTemplate_Job in CreateTemplate_Jobs:
+            dax.addJob(CreateTemplate_Job.pegasus_job)
+
+        for CreateTemplate_Job in CreateTemplate_Jobs:
+            for ImageDim_Job in ImageDim_Jobs:
+                dax.depends(parent=ImageDim_Job.pegasus_job, child=CreateTemplate_Job.pegasus_job)
+
+        #Possibly go deeper
+        if len(hierarchies) > 2:
+            child_matrix_set = matrix[hierarchies[1:]]
+            for child_group, child_matrix in child_matrix_set.groupby(hierarchies[1]):
+                dax, parents = self.__plan_tier__(child_matrix[hierarchies[1:]+["SPD"]], hierarchies[1:], dax)
+                for parent in parents:
+                    for ImageDim_Job in ImageDim_Jobs:
+                        dax.depends(parent=parent.pegasus_job, child=ImageDim_Job.pegasus_job)
+
+        #Will eventually return the last step in normalization for each tier
+        return dax, CreateTemplate_Jobs
+
 
 
     def add_to_dax(self, dax):
+        dax, parents = self.__plan_tier__(self.matrix, self.hierarchy, dax)
+        self.parents = parents
         return dax
 
+    def save_files(self, root):
+        for filename, contents in self.files.iteritems():
+            with open(root+"/"+filename, "w") as f:
+                f.write(contents)
+
 class normalize_ImageDim(object):
-    def __init__(self, template_tier, source_tier, template_id, source_id, hierarchy, matrix):
-        self.job_id = "Normalize_ImageDim_{0}-{1}".format(source_tier, source_id)
+    def __init__(self, template_tier, source_tier, template_id, source_id, hierarchy, matrix, transferflag):
+        self.job_id = "{0}-{1}".format(source_tier, source_id)
         self.pegasus_job = Job(name="Normalize_ImageDim", namespace="dipa", id=self.job_id)
+        self.files = {}
         #Check to see if this is the basic level of the normalization.
         #If so, use the SPD specified. Otherwise, input is the output of a previous step.
-        if source_id == hierarchy[-1]:
-            inputfile = matrix[matrix[template_tier] == template_id][matrix[source_tier] == source_id]["SPD"]
+        if source_tier == hierarchy[-1]:
+            inputfile = list(matrix[matrix[template_tier] == template_id][matrix[source_tier] == source_id]["SPD"])[0]
         else:
             inputfile = "{0}-{1}_template.nii.gz".format(source_tier, source_id)
         outputfile = "{0}_dimensions.nii.gz".format(source_id)
         args = ["--id={0}".format(source_id), "--inputfile="+inputfile, "--outputfile="+outputfile]
         self.pegasus_job.uses(inputfile, link=Link.INPUT)
-        self.pegasus_job.uses(outputfile, link=Link.OUTPUT, transfer=DEFAULT_INTERMEDIATE_FILES_TRANSFER_FLAG)
-        self.addArguments(*args)
+        self.pegasus_job.uses(outputfile, link=Link.OUTPUT, transfer=transferflag)
+        self.pegasus_job.addArguments(*args)
 
 class normalize_CreateTemplate(object):
-    def __init__(self, template_tier, source_tier, template_id, hierarchy, matrix):
-        self.job_id = "Normalize_CreateTemplate_{0}-{1}".format(template_tier, template_id)
+    def __init__(self, template_tier, source_tier, template_id, hierarchy, matrix, transferflag):
+        self.job_id = "{0}-{1}".format(template_tier, template_id)
         self.pegasus_job = Job(name="Normalize_CreateTemplate", namespace="dipa", id=self.job_id)
+        self.files = {}
+
 
         self.pegasus_job.addArguments(
                        "--lookupfile", "{0}_dimension_files.csv".format(template_id),
@@ -88,14 +135,26 @@ class normalize_CreateTemplate(object):
                         "{0}_mean_rigid0.nii.gz".format(template_id)]
 
         if source_tier == "ID":
-            input_files.append(list(matrix[matrix[template_tier] == template_id]["SPD"]))
+            spd_inputs = list(matrix[matrix[template_tier] == template_id]["SPD"])
         else:
+            spd_inputs = []
             for source_id in source_ids:
-                input_files.append("{0}-{1}_template.nii.gz".format(source_tier, source_id))
+                spd_inputs.append("{0}-{1}_template.nii.gz".format(source_tier, source_id))
+
+        initial_template_input_text = "\n".join(spd_inputs)
+        input_files.extend(spd_inputs)
+        input_csv_contents = matrix[[source_tier]]
+        files = input_csv_contents[source_tier].apply(lambda row: "{0}_dimensions.nii.gz".format(row))
+        ids = input_csv_contents[source_tier]
+        contents = pandas.DataFrame({"ID":ids, "FILE":files})
+        input_csv_text = contents.to_csv(columns=["ID","FILE"], index=False)
+
+        self.files["{0}_initial_template_input.txt".format(template_id)] = initial_template_input_text
+        self.files["{0}_dimension_files.csv".format(template_id)] = input_csv_text
         for source_id in source_ids:
             input_files.append("{0}_dimensions.nii.gz".format(source_id))
 
         for inputfile in input_files:
             self.pegasus_job.uses(inputfile, link=Link.INPUT)
         for outputfile in output_files:
-            self.pegasus_job.uses(outputfile, link=Link.OUTPUT, transfer=DEFAULT_INTERMEDIATE_FILES_TRANSFER_FLAG)
+            self.pegasus_job.uses(outputfile, link=Link.OUTPUT, transfer=transferflag)
--- dipa.py
+++ dipa.py
@@ -9,6 +9,7 @@ import pandas
 import csv
 import json
 from docopt import docopt
+from components.normalize import normalize
 
 __version__ = "0.3b"
 __doc__ = """Diffusion Image Processing and Analysis. v{0}
@@ -30,6 +31,7 @@ Options:
      -p <path> --project <path>      The output directory.
      -s <site> --site <site>         Site from site catalog to run the workflow.
      -k --keep_files                 Keep intermediate files. [default: False]
+     -n <str> --name <str>           Add a custom name for the project. [default: Project]
      --correct_type                  (Eddy Correction) Select 'eddy' or 'eddy_correct'. [default: eddy]
      --fit_method                    (Model Fitting) Select either 'WLS' (weighted least squares) or 'OLS' (ordinary least squares) [default: WLS]
      --template                      (Normalization) Designate an original template (optional). [default: None]
@@ -38,13 +40,14 @@ Options:
      --rigid <rigidcount>            (Normalization) Number of rigid iterations. [default: 3]
      --affine <affinecount>          (Normalization) Number of affine iterations. [default: 3]
      --diffeo <diffeocount>          (Normalization) Number of diffeomorphic iterations. [default: 6]
-     --tier <level>                  (Normalization) Normalize Hierarchichally. [default: Project]
+     --tier <level>                  (Normalization) Normalize Hierarchichally. [default: PROJECT]
      --export_style <style>          (ROI Extraction) Specify 'long', 'wide' or 'both' [default: long]
 """.format(__version__)
 
 __arg_mapping__ = {
  "--input": "InputFile",
  "--project": "ProjectDir",
+ "--name": "ProjectName",
  "--site": "Site",
  "--keep_files": "KeepFiles",
  "--correct_type": "CorrectType",
@@ -100,152 +103,166 @@ def read_json(jsonpath):
         sys.exit(1)
 
 
-def getDAX( normalize_file, input_dir ):
+def create_workflow(options):
     """
     This generates a dipa dax
     """
 
     dax = ADAG( "dipa" )
 
-    individuals = readNormalizeFile( normalize_file )
+    raw_matrix = pandas.read_csv(clean_path(options["InputFile"]))
 
-    imagedim_jobs=[]
-    for individual in individuals:
-        #add the location of _spd.nii.gz as Files in the DAX
-        lfn = individual[0] + "_spd.nii.gz"
-        file = File(lfn)
-        file.addPFN( PFN("file://" + individual[1], "waisman"))
-        dax.addFile( file )
+    for index, row in raw_matrix.iterrows():
+        try:
+            shutil.copyfile(row["SPD"], options["ProjectDir"]+"/input/"+"{0}_spd.nii.gz".format(row["ID"]))
+        except:
+            print("Either your input csv was ill-formatted, or some number of files do not exist.")
+            sys.exit(1)
 
-        # for each individual we construct a ImageDim_Project job
-        imagedim_job = construct_image_dim_job( individual[0] )
-        dax.addJob( imagedim_job )
-        imagedim_jobs.append( imagedim_job )
+    matrix = raw_matrix.copy()
+    matrix["SPD"] = matrix["ID"].apply(lambda row: "{0}_spd.nii.gz".format(row))
 
-    # create the CreateTemplate_Project job.
-    # requires some input files that dax generator creates currently image_dimension_files.csv initial_template_input.txt
-    image_dimension_files_csv = write_image_dimensions_file(input_dir, individuals)
-    file = File( os.path.basename(image_dimension_files_csv))
-    file.addPFN( PFN("file://" +image_dimension_files_csv, "waisman"))
-    dax.addFile( file )
+    normalize_section = normalize(matrix, hierarchy=options["NormalizeHierarchy"], name=options["ProjectName"])
+    dax = normalize_section.add_to_dax(dax)
+    normalize_section.save_files(options["ProjectDir"]+"/input/")
 
-    #write out initial_template_input.txt
-    initial_template_input_txt = write_initial_template_input_file(input_dir, individuals)
-    file = File( os.path.basename(initial_template_input_txt))
-    file.addPFN( PFN("file://" +initial_template_input_txt, "waisman"))
-    dax.addFile( file )
-
-    template_project_job = construct_template_project_job( individuals )
-    dax.addJob( template_project_job )
-
-    # the createtemplate job depends on all of imagedim jobs
-    for parent_job in imagedim_jobs:
-        dax.depends( parent=parent_job, child=template_project_job )
+    # imagedim_jobs=[]
+    # for individual in individuals:
+    #     #add the location of _spd.nii.gz as Files in the DAX
+    #     lfn = individual[0] + "_spd.nii.gz"
+    #     file = File(lfn)
+    #     file.addPFN( PFN("file://" + individual[1], "waisman"))
+    #     dax.addFile( file )
+    #
+    #     # for each individual we construct a ImageDim_Project job
+    #     imagedim_job = construct_image_dim_job( individual[0] )
+    #     dax.addJob( imagedim_job )
+    #     imagedim_jobs.append( imagedim_job )
+    #
+    # # create the CreateTemplate_Project job.
+    # # requires some input files that dax generator creates currently image_dimension_files.csv initial_template_input.txt
+    # image_dimension_files_csv = write_image_dimensions_file(input_dir, individuals)
+    # file = File( os.path.basename(image_dimension_files_csv))
+    # file.addPFN( PFN("file://" +image_dimension_files_csv, "waisman"))
+    # dax.addFile( file )
+    #
+    # #write out initial_template_input.txt
+    # initial_template_input_txt = write_initial_template_input_file(input_dir, individuals)
+    # file = File( os.path.basename(initial_template_input_txt))
+    # file.addPFN( PFN("file://" +initial_template_input_txt, "waisman"))
+    # dax.addFile( file )
+    #
+    # template_project_job = construct_template_project_job( individuals )
+    # dax.addJob( template_project_job )
+    #
+    # # the createtemplate job depends on all of imagedim jobs
+    # for parent_job in imagedim_jobs:
+    #     dax.depends( parent=parent_job, child=template_project_job )
 
     return dax
 
 
-
-
-def construct_image_dim_job( individual_id):
-
-    j = Job(name="Normalize_ImageDim",namespace="dipa", id="ID" + individual_id,)
-
-    #--id=ID103414 --inputfile=103414_spd.nii.gz --outputfile=103414_spd_dimensions.csv
-    args = []
-    args.append( "--id=ID" + individual_id  )
-    args.append( "--inputfile=" + individual_id + "_spd.nii.gz")
-    args.append( "--outputfile=" + individual_id + "_spd_dimensions.csv")
-
-
-    input_file = individual_id + "_spd.nii.gz"
-    j.uses( input_file , link=Link.INPUT)
-
-    #add the output log files
-    output_file = individual_id + "_spd_dimensions.csv"
-    j.uses( output_file, link=Link.OUTPUT, transfer=DEFAULT_INTERMEDIATE_FILES_TRANSFER_FLAG)
-
-    # Finish job
-    j.addArguments(*args)
-
-    return j
-
-
-def construct_template_project_job( individuals):
-
-    j = Job(name="Normalize_CreateTemplate",namespace="dipa")
-
-    args = []
-
-    #add input files per individual
-    for individual in individuals:
-        individual_id = individual[0]
-        for suffix in ["_spd.nii.gz", "_spd_dimensions.csv"]:
-            input_file = individual_id + suffix
-            j.uses( input_file , link=Link.INPUT)
-
-    #add the fixed input files image_dimension_files.csv , initial_template_input.txt
-    for input_file in ["initial_template_input.txt", "image_dimension_files.csv" ]:
-        j.uses(  input_file , link=Link.INPUT)
-
-
-    #add the output files
-    for output_file in ["template_vsize.txt", "template_resample_bool.txt", "template_iso_vsize.txt", "template_dim.txt", "initial_template_orig.nii.gz", "initial_template.nii.gz" ,"mean_rigid0.nii.gz"]:
-        j.uses( output_file, link=Link.OUTPUT, transfer=DEFAULT_INTERMEDIATE_FILES_TRANSFER_FLAG)
-
-    # Finish job
-    j.addArguments("--lookupfile", "image_dimension_files.csv",
-                   "--templateinputsfile", "initial_template_input.txt",
-                   "--resamplefile", "template_resample_bool.txt",
-                   "--vsizefile", "template_vsize.txt",
-                   "--dimsizefile", "template_dim.txt",
-                   "--orig", "initial_template_orig.nii.gz",
-                   "--initial", "initial_template.nii.gz",
-                   "--rigid", "mean_rigid0.nii.gz",)
-
-    return j
-
-def readNormalizeFile( input_file ):
-    """
-    Reads in the normalize input file and returns a list of tuples with id's and file locations.
-    :param input_file:
-    :return:
-    """
-
-    print input_file
-    result = []
-    with open( input_file, 'rb') as f:
-        reader = csv.reader(f)
-        result = list(reader)
-
-    if len(result) == 0:
-        #empty file. just return
-        return result
-
-    #remove the header if specified
-    header = result[0]
-    if header[0] == "ID":
-        #remove the first entry from result list
-        del result[0]
-
-    return result
-
-def write_image_dimensions_file( directory, individuals ):
-
-    file = open( directory + "/" + "image_dimension_files.csv", 'w')
-    file.write("ID,File" + "\n")
-    for individual in individuals:
-        file.write( individual[0] + "," + individual[0] + "_spd_dimensions.csv" + "\n")
-    file.close()
-    return file.name
-
-def write_initial_template_input_file( directory, individuals ):
-
-    file = open( directory + "/" + "initial_template_input.txt", 'w')
-    for individual in individuals:
-        file.write( individual[0] + "_spd.nii.gz" + "\n")
-    file.close()
-    return file.name
+#
+#
+# def construct_image_dim_job( individual_id):
+#
+#     j = Job(name="Normalize_ImageDim",namespace="dipa", id="ID" + individual_id,)
+#
+#     #--id=ID103414 --inputfile=103414_spd.nii.gz --outputfile=103414_spd_dimensions.csv
+#     args = []
+#     args.append( "--id=ID" + individual_id  )
+#     args.append( "--inputfile=" + individual_id + "_spd.nii.gz")
+#     args.append( "--outputfile=" + individual_id + "_spd_dimensions.csv")
+#
+#
+#     input_file = individual_id + "_spd.nii.gz"
+#     j.uses( input_file , link=Link.INPUT)
+#
+#     #add the output log files
+#     output_file = individual_id + "_spd_dimensions.csv"
+#     j.uses( output_file, link=Link.OUTPUT, transfer=DEFAULT_INTERMEDIATE_FILES_TRANSFER_FLAG)
+#
+#     # Finish job
+#     j.addArguments(*args)
+#
+#     return j
+#
+#
+# def construct_template_project_job( individuals):
+#
+#     j = Job(name="Normalize_CreateTemplate",namespace="dipa")
+#
+#     args = []
+#
+#     #add input files per individual
+#     for individual in individuals:
+#         individual_id = individual[0]
+#         for suffix in ["_spd.nii.gz", "_spd_dimensions.csv"]:
+#             input_file = individual_id + suffix
+#             j.uses( input_file , link=Link.INPUT)
+#
+#     #add the fixed input files image_dimension_files.csv , initial_template_input.txt
+#     for input_file in ["initial_template_input.txt", "image_dimension_files.csv" ]:
+#         j.uses(  input_file , link=Link.INPUT)
+#
+#
+#     #add the output files
+#     for output_file in ["template_vsize.txt", "template_resample_bool.txt", "template_iso_vsize.txt", "template_dim.txt", "initial_template_orig.nii.gz", "initial_template.nii.gz" ,"mean_rigid0.nii.gz"]:
+#         j.uses( output_file, link=Link.OUTPUT, transfer=DEFAULT_INTERMEDIATE_FILES_TRANSFER_FLAG)
+#
+#     # Finish job
+#     j.addArguments("--lookupfile", "image_dimension_files.csv",
+#                    "--templateinputsfile", "initial_template_input.txt",
+#                    "--resamplefile", "template_resample_bool.txt",
+#                    "--vsizefile", "template_vsize.txt",
+#                    "--dimsizefile", "template_dim.txt",
+#                    "--orig", "initial_template_orig.nii.gz",
+#                    "--initial", "initial_template.nii.gz",
+#                    "--rigid", "mean_rigid0.nii.gz",)
+#
+#     return j
+#
+# def readNormalizeFile( input_file ):
+#     """
+#     Reads in the normalize input file and returns a list of tuples with id's and file locations.
+#     :param input_file:
+#     :return:
+#     """
+#
+#     print input_file
+#     result = []
+#     with open( input_file, 'rb') as f:
+#         reader = csv.reader(f)
+#         result = list(reader)
+#
+#     if len(result) == 0:
+#         #empty file. just return
+#         return result
+#
+#     #remove the header if specified
+#     header = result[0]
+#     if header[0] == "ID":
+#         #remove the first entry from result list
+#         del result[0]
+#
+#     return result
+#
+# def write_image_dimensions_file( directory, individuals ):
+#
+#     file = open( directory + "/" + "image_dimension_files.csv", 'w')
+#     file.write("ID,File" + "\n")
+#     for individual in individuals:
+#         file.write( individual[0] + "," + individual[0] + "_spd_dimensions.csv" + "\n")
+#     file.close()
+#     return file.name
+#
+# def write_initial_template_input_file( directory, individuals ):
+#
+#     file = open( directory + "/" + "initial_template_input.txt", 'w')
+#     for individual in individuals:
+#         file.write( individual[0] + "_spd.nii.gz" + "\n")
+#     file.close()
+#     return file.name
 
 def system_call(command, environment={}):
   #A shorthand for running system processes, such as FSL commands
@@ -286,7 +303,7 @@ def main():
 
 
     #Create the DAX
-    dax = getDAX(options["InputFile"], options["ProjectDir"]+"/input")
+    dax = create_workflow(options)
     with open( options["DaxFile"],"w" ) as f:
         print "Writing DAX to {0}".format(options["DaxFile"])
         dax.writeXML(f)
